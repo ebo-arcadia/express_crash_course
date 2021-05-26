9fe6cd6514e29ce664fd75194a63bb0d55e41be6
Ebo Lee <ebolee@Ebos-MacBook-Air.local>
2021-05-25 22:08:58 -0700

    using post man to process get request for memebrs data

--- index.js
+++ index.js
@@ -3,12 +3,30 @@ const path = require('path')
 
 const app = express();
 
+const members = [
+    {
+        id: 1,
+        name: 'Jay Choi',
+        email: 'jay@email.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Neo Anderson',
+        email: 'neo@email.com',
+        status: 'inactive'
+    }
+];
+
+// create routes to get all members
+app.get('/api/members', (request, response) => {response.json(members);})
+
 // static folder
 // app.get('/', (request, response) => {
 //     response.send(path.join(__dirname, 'public', 'index.html'));
 // });
 
-// set static folder
+// set static folder / server
 app.use(express.static(path.join(__dirname, 'public')))
 
 const PORT = process.env.PORT || 5000;
